6921b89ee80476dd3f58d3bf27cb564755e1310b
zhewang711 <zhw176@ucsd.edu>
2016-04-24 17:47:40 -0700

    most of stub work

---
+++ project1/rmi/MyHandler.java
@@ -0,0 +1,49 @@
+package rmi;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.net.*;
+import java.io.*;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by zhewang711 on 4/24/16.
+ */
+public class MyHandler implements InvocationHandler{
+
+    private InetSocketAddress address;
+
+    public MyHandler(InetSocketAddress address){
+        this.address = address;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Socket socket = new Socket(this.address.getHostName(), this.address.getPort());
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.flush();
+        oos.writeObject(method.getName());
+        oos.writeObject(method.getParameterTypes());
+        oos.writeObject(args);
+
+        while (socket.getInputStream().available() <= 0)
+            sleep(1);
+
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        Object res = ois.readObject();
+        oos.close();
+        ois.close();
+        socket.close();
+
+        if (res instanceof Exception ){
+            throw (Exception)res;
+        }
+        else {
+            return res;
+        }
+
+    }
+
+}

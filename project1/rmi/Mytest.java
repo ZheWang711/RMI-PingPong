c09e77734d32d2b89005ae222d996d370a6d8435
zhewang711 <zhw176@ucsd.edu>
2016-04-27 16:03:01 -0700

    toString and some tests

---
+++ project1/rmi/Mytest.java
@@ -0,0 +1,63 @@
+package rmi;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by zhewang711 on 4/27/16.
+ */
+public class Mytest {
+
+    public static void main(String [] args){
+        testVoid();
+        testVoid();
+    }
+
+    private static void startServer(){
+        TestInterfaceImpl si = new TestInterfaceImpl();
+        Skeleton<TestInterface> skeleton = new Skeleton(TestInterface.class, si);
+        try {
+            skeleton.start();
+        }
+        catch (RMIException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static TestInterface startStub(){
+        InetSocketAddress address = new InetSocketAddress("localhost", 7000);
+        TestInterface s = Stub.create(TestInterface.class, address);
+        return s;
+    }
+
+    private static void testToString(){
+        InetSocketAddress address = new InetSocketAddress("localhost", 7000);
+        TestInterface s = Stub.create(TestInterface.class, address);
+        System.out.println(s);
+    }
+
+    private static void testNull(){
+
+        startServer();
+        TestInterface s = startStub();
+
+        try{
+            System.out.println(s.captl( null, null));
+        }
+        catch (RMIException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void testVoid(){
+        startServer();
+        TestInterface s = startStub();
+        try{
+            s.echo();
+        }
+        catch (RMIException e){
+            e.printStackTrace();
+        }
+    }
+
+}
